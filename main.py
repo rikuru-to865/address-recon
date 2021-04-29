2aa2f335e2ae936205aff1dc9e156325888358fb
rikuru-to865 <usaginosippo865@gmail.com>
2021-04-30 00:24:18 +0900

    heroku

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 from flask import Flask, render_template,request
+import json
+import requests
+import os
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    print(app.logger())
     return render_template("index.html")
 
+@app.route("/address",methods=["POST"])
+def address():
+    info = request.get_data()
+    requests.post(os.getenv("webhook"),"content="+info,headers={'content-type',
+      'application/x-www-form-urlencoded;charset=UTF-8'})
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
